adad9aa77a6f09014431ca191c637e7f3c0d49fd
heart-original <1316305052@qq.com>
2020-10-16 15:18:11 +0800

    second day(1):Eureka

--- cloud-consumer-order80/src/main/java/com/maple/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/maple/springcloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.maple.springcloud.entities.CommonResult;
 import com.maple.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +14,8 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-    public static final String PAMENT_URL = "http://localhost:8001";
+    //public static final String PAMENT_URL = "http://localhost:8001";
+    public static final String PAMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
@@ -21,5 +23,10 @@ public class OrderController {
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAMENT_URL + "/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> create(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAMENT_URL + "/payment/get/"+id,CommonResult.class);
     }
 }
