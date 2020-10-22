240195c6682d123807421a25a9c5250a84911a5c
heart-original <1316305052@qq.com>
2020-10-22 16:34:55 +0800

    third day(2):ribbon

--- cloud-consumer-order80/src/main/java/com/maple/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/maple/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.maple.springcloud.controller;
 import com.maple.springcloud.entities.CommonResult;
 import com.maple.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,19 @@ public class OrderController {
     }
 
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> create(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAMENT_URL + "/payment/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id){
+        //return restTemplate.getForObject(PAMENT_URL + "/payment/get/"+id,CommonResult.class);
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAMENT_URL + "/payment/get/"+id,CommonResult.class);
+
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444 ,"操作失败");
+        }
     }
 }
