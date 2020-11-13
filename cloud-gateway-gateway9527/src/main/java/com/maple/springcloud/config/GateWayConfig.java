a805dfa2201bdad668e5ed5f96a5110079f45c68
heart-original <1316305052@qq.com>
2020-11-13 14:22:46 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/maple/springcloud/config/GateWayConfig.java
@@ -0,0 +1,19 @@
+package com.maple.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customeRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route_maple",
+                r->r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei"));
+        return routes.build();
+    }
+}
